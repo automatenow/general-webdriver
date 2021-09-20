bc3c6a303d5520b35d08d336b862c07262eb7e79
Marco Cruz <info@automatenow.io>
2021-09-19 20:36:36 -0400

    Refactored the testSoftAssert() method

--- src/test/java/io/automatenow/tests/SoftAssertions.java
+++ src/test/java/io/automatenow/tests/SoftAssertions.java
@@ -1,26 +1,21 @@
 package io.automatenow.tests;
 
 import io.automatenow.utils.CustomSoftAssert;
-import io.automatenow.utils.TestListener;
 import org.testng.annotations.*;
 
 /**
  * @author Marco A. Cruz
  */
-@Listeners(TestListener.class)
 public class SoftAssertions extends BaseTest {
+    public static CustomSoftAssert softAssert = new CustomSoftAssert();
 
     @Test
     public void testSoftAssert() {
-        // First soft assertion checks the page title
         String pageTitle = homePage.getPageTitle();
-        CustomSoftAssert softAssert = new CustomSoftAssert();
-        softAssert.assertEquals(pageTitle, "wrong title", "The tile is invalid");
-//        softAssert.assertEquals(pageTitle, "Home - automateNow");
+        softAssert.assertEquals(pageTitle, "Homes - automateNow", "The title is invalid");
 
-        // Second soft assertion checks the welcome message
         String greeting = homePage.getWelcomeMessage();
-        softAssert.assertTrue(greeting.contains("Welcomes"), "The greeting does not contain 'Welcome'");
+        softAssert.assertTrue(greeting.contains("Welcomes"), "The greeting message does not contain 'Welcome'");
 
         softAssert.assertAll();
     }
